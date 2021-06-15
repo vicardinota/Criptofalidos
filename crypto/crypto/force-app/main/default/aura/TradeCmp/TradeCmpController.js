7b86cbcda8ead3c62ae107de983eba5ec68f114a
Alan Teles <alanlimateles@yahoo.com.br>
2021-06-15 03:13:04 +0200

    Adicionada integração para Binance

--- crypto/crypto/force-app/main/default/aura/TradeCmp/TradeCmpController.js
+++ crypto/crypto/force-app/main/default/aura/TradeCmp/TradeCmpController.js
@@ -1,5 +1,28 @@
 ({
-    myAction : function(component, event, helper) {
+    retrievePrice : function(component, event, helper) {
+        var action = component.get("c.getPrice");
+        action.setParams({
+            "mercadoId" : component.get("v.recordId")
+        });
 
+        action.setCallback(this, function(response){
+            var state = response.getState();
+            if(state == "SUCCESS"){
+               alert('O preço atual é: ' + response.getReturnValue());
+            }else{
+                console.log("Erro: " + JSON.stringify(response.getError()));
+            }
+        });
+
+        $A.enqueueAction(action);
+    },
+
+    handleChange : function(component, event, helper) {
+        //console.log(event.getParam("checked"));
+        component.set("v.opMercado", !event.getParam("checked"));
+    },
+
+    handleOrder : function(component, event, helper) {
+        alert('Código de salvar a ordem');
     }
 })
