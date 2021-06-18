d6c66ac71f2a55bb79f6348e51898436580019d6
Alan Teles <alanlimateles@yahoo.com.br>
2021-06-18 02:59:58 +0200

    Finalizando componente de ordem

--- crypto/crypto/force-app/main/default/aura/TradeCmp/TradeCmpController.js
+++ crypto/crypto/force-app/main/default/aura/TradeCmp/TradeCmpController.js
@@ -1,14 +1,52 @@
 ({
     retrievePrice : function(component, event, helper) {
-        var action = component.get("c.getPrice");
+        helper.retrievePrice(component, event, helper);
+    },
+
+    handleChange : function(component, event, helper) {
+        //console.log(event.getParam("checked"));
+        component.set("v.opMercado", !event.getParam("checked"));
+        helper.retrievePrice(component, event, helper);
+    },
+
+    handleSuccess : function(component, event, helper){
+        var toastEvent = $A.get("e.force:showToast");
+        toastEvent.setParams({
+            "title" : 'Sucesso!',
+            "message" : 'Registro inserido!',
+            "type" : "success",
+            "duration" : 3000
+        });
+        toastEvent.fire();
+
+        component.find("ordField").forEach(function(f){
+            f.reset();
+        });
+    },
+
+    handleOrder : function(component, event, helper) {
+        event.preventDefault();
+        var fields = event.getParam("fields");
+        
+        var action = component.get("c.getRecordTypeId");
         action.setParams({
-            "mercadoId" : component.get("v.recordId")
+            "orderType" : "Compra"
         });
 
         action.setCallback(this, function(response){
             var state = response.getState();
             if(state == "SUCCESS"){
-               alert('O preço atual é: ' + response.getReturnValue());
+               var rtId = response.getReturnValue();
+               fields["RecordTypeId"] = rtId;
+               if(fields["OperacaoMercado__c"] == false){
+                    fields["Status__c"] = "Aguardando";
+               }else{
+                    fields["Status__c"] = "Executada";
+               }
+               if(fields["Preco__c"] == undefined){
+                    fields["Preco__c"] = component.get("v.currentPrice");
+               }
+               component.find('orderForm').submit(fields);
             }else{
                 console.log("Erro: " + JSON.stringify(response.getError()));
             }
@@ -17,12 +55,19 @@
         $A.enqueueAction(action);
     },
 
-    handleChange : function(component, event, helper) {
-        //console.log(event.getParam("checked"));
-        component.set("v.opMercado", !event.getParam("checked"));
+    handleQuantityPriceChange : function(component, event, helper) {
+        var price = component.get("v.price");
+        var quantity = component.get("v.quantity");
+        var total = price*quantity;
+
+        component.set("v.total", total);
     },
 
-    handleOrder : function(component, event, helper) {
-        alert('Código de salvar a ordem');
+    handleTotalChange : function(component, event, helper) {
+        var price = component.get("v.price");
+        var total = component.get("v.total");
+        var quantity = total/price;
+
+        component.set("v.quantity", quantity);
     }
 })
